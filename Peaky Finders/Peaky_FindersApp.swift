5a9fa9a17aeb712af84a98d0d8c10ccb483c4413
amorris <amorris@amorris.cc>
2026-06-03 12:29:31 -0400

    Initial Commit

---
+++ Peaky Finders/Peaky_FindersApp.swift
@@ -0,0 +1,17 @@
+//
+//  Peaky_FindersApp.swift
+//  Peaky Finders
+//
+//  Created by Albert Morris on 6/3/26.
+//
+
+import SwiftUI
+
+@main
+struct Peaky_FindersApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
