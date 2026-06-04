f8936f05ca9b13222da82b462527c87f2c4ae44d
amorris <amorris@amorris.cc>
2026-06-04 09:19:36 -0400

    Reading location and heading.

---
+++ Peaky Finders/LocationProvider.swift
@@ -0,0 +1,48 @@
+//
+//  LocationProvider.swift
+//  Peaky Finders
+//
+//  Created by Albert Morris on 6/4/26.
+//
+
+import Foundation
+import CoreLocation
+import Observation
+
+@Observable
+final class LocationProvider: NSObject, CLLocationManagerDelegate {
+    private let manager = CLLocationManager()
+    
+    var authorizationStatus: CLAuthorizationStatus  = .notDetermined
+    var currentLocation: CLLocation?
+    var heading: Double?
+    
+    override init() {
+        super.init()
+        manager.delegate = self
+        manager.desiredAccuracy = kCLLocationAccuracyBest
+        authorizationStatus = manager.authorizationStatus
+    }
+    
+    func start() {
+        manager.requestWhenInUseAuthorization()
+        manager.startUpdatingLocation()
+        manager.startUpdatingHeading()
+    }
+    
+    func locationManagerDidChangeAuthorization(_ manager: CLLocationManager) {
+        authorizationStatus = manager.authorizationStatus
+    }
+    
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        currentLocation = locations.last
+    }
+    
+    func locationManager(_ manager: CLLocationManager, didUpdateHeading newHeading: CLHeading) {
+        heading = newHeading.trueHeading >= 0 ? newHeading.trueHeading : newHeading.magneticHeading
+    }
+    
+    func locationManager(_ manager: CLLocationManager, didFailWithError error: any Error) {
+        print("Core Location Error: \(error.localizedDescription)")
+    }
+}
